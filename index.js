9adefc6e4e79078206a9edbe4dca1eef35f9ccda
marcus <95848098+itskaefer@users.noreply.github.com>
2022-11-22 13:14:35 +0100

    ready for first test

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var exec = require('child_process').exec;
 var nodetools = require('nodetools');
 
 module.exports = ControllerInputDefaults;
+
 function ControllerInputDefaults(context) {
 	// This fixed variable will let us refer to 'this' object at deeper scopes
 	var self = this;
@@ -18,11 +19,14 @@ function ControllerInputDefaults(context) {
 	this.logger = this.context.logger;
 	this.configManager = this.context.configManager;
 
+  // TODO
+  // for different miniDSP Models
+  // available inputs
+  self.inputs = ['TOSLINK','SPDIF','AES-EBU','RCA','XLR','USB','LAN'];
 }
 
 
-ControllerInputDefaults.prototype.onVolumioStart = function()
-{
+ControllerInputDefaults.prototype.onVolumioStart = function() {
 	var self = this;
 	var defer=libQ.defer();
 	self.logger.info("[input-defaults] plugin initialized");
@@ -30,28 +34,51 @@ ControllerInputDefaults.prototype.onVolumioStart = function()
 	this.config = new (require('v-conf'))();
 	this.config.loadFile(configFile);
 
-        // var promise = libQ.nfcall(fs.writeFile, '/tmp/message', 'Hello World', 'utf8');
+  // var promise = libQ.nfcall(fs.writeFile, '/tmp/message', 'Hello World', 'utf8');
 
-        return defer.promise;;
-}
+  return defer.promise;
+};
 
-ControllerInputDefaults.prototype.onStart = function()
-{
+ControllerInputDefaults.prototype.onStart = function() {
 	var self = this;
 	var defer = libQ.defer();
 	return defer.promise;
-}
+};
 
 
-ControllerInputDefaults.prototype.onStop = function()
-{
+ControllerInputDefaults.prototype.onStop = function() {
 	var self = this;
 	var defer = libQ.defer();
 	return defer.promise;
-}
+};
 
-ControllerInputDefaults.prototype.setDefaultValues = function(input)
+ControllerInputDefaults.prototype.getConfigurationFiles = function()
 {
+	return ['config.json'];
+};
+
+ControllerInputDefaults.prototype.getConf = function(configFile) {
+	var self = this;
+	this.config = new (require('v-conf'))();
+	this.config.loadFile(configFile);
+	return libQ.resolve();
+};
+
+ControllerInputDefaults.prototype.onInstall = function() {
+	var self = this;
+  self.logger.info("[input-defaults] performing onInstall action");
+};
+
+ControllerInputDefaults.prototype.onUninstall = function()
+{
+  var self = this;
+  self.logger.info("[input-defaults] performing onUninstall action");
+};
+
+
+// get actual choosen input from /music_service/inputs plugin
+// and set the volume and preset according to plugin settings
+ControllerInputDefaults.prototype.setDefaultValues = function(input) {
 	var self = this;
 	var defer = libQ.defer();
 
@@ -59,13 +86,72 @@ ControllerInputDefaults.prototype.setDefaultValues = function(input)
 	this.config = new (require('v-conf'))();
 	this.config.loadFile(configFile);
 
-	var volume = self.config.get(input + "_volume");
-	var preset = self.config.get(input + "_preset");
+	var volume = self.config.get(input + "-volume");
+	var preset = self.config.get(input + "-preset");
 
 	self.commandRouter.executeOnPlugin('music_service','inputs','setPreset', preset );
 	self.commandRouter.volumiosetvolume.call(self.commandRouter, volume);
 
+	return defer.promise;
+};
+
+
+ControllerInputDefaults.prototype.getUIConfig = function() {
+  var self = this;
+	var defer = libQ.defer();
+  var lang_code = this.commandRouter.sharedVars.get('language_code');
+	self.getConf(this.configFile);
+
+  var presetList = ["1","2","3","4"];
+
+  // TODO
+  // var volumeOptions = ['1', .. ,'255'];
+
+  self.commandRouter.i18nJson(__dirname+'/i18n/strings_' + lang_code + '.json',
+                            	__dirname + '/i18n/strings_en.json',
+                            	__dirname + '/UIConfig.json')
+  .then(function(uiconf)
+  {
+    self.logger.info("[input-defaults] Populating UI for configuration...");
+    var i = 0;
+    for ( var input in self.inputs ) {
+      uiconf.sections[0].content[i++].value = self.config.get(input + '-volume');
+
+      // add preset options and show actual preset from config-file
+      for ( var preset in presetList ) {
+        self.configManager.pushUIConfigParam(uiconf, 'sections[0].content['+ i +'].options', {
+  				value: preset,
+  				label: 'Preset ' + preset
+			  });
+      }
+      uiconf.sections[0].content[i++].value = self.config.get(input + '-preset');
+    }
+		self.logger.info("[input-defaults] settings loaded");
+    defer.resolve(uiconf);
+  })
+  .fail(function()
+  {
+    defer.reject(new Error());
+  });
 
 	return defer.promise;
-}
+};
 
+ControllerInputDefaults.prototype.setUIConfig = function(data) {
+	var self = this;
+	var uiconf = fs.readJsonSync(__dirname + '/UIConfig.json');
+	return libQ.resolve();
+};
+
+// get values from plugin settings page and update config.json
+ControllerInputDefaults.prototype.updateInputDefaultsConfig = function(data) {
+  var self = this;
+	var defer = libQ.defer();
+
+  for ( var input in self.inputs ) {
+    self.config.set(input + '-volume', data[input + '-volume']);
+    self.config.set(input + '-preset', data[input + '-preset']);
+  }
+ 	self.logger.info("[input-defaults] Successfully updated configuration");
+  return defer.promise;
+};
--- install.sh
+++ install.sh
@@ -5,28 +5,23 @@ INSTALLING="/home/volumio/input-defaults-plugin.installing"
 if [ ! -f $INSTALLING ]; then
 	/bin/touch $INSTALLING
 
-	if [ ! -d /opt/squeezelite ]; then
-		dist=$(cat /etc/os-release | grep '^VERSION=' | cut -d '(' -f2 | tr -d ')"')
-		arch=$(arch)
-    		variant=$(cat /etc/os-release | grep '^VOLUMIO_VARIANT=' | cut -d '=' -f2 | tr -d '"')
+	dist=$(cat /etc/os-release | grep '^VERSION=' | cut -d '(' -f2 | tr -d ')"')
+	arch=$(arch)
+  variant=$(cat /etc/os-release | grep '^VOLUMIO_VARIANT=' | cut -d '=' -f2 | tr -d '"')
 
-    		# Fix executable rights
-		#chown volumio:volumio /opt/squeezelite
-		#chmod 755 /opt/squeezelite
+  if [ $variant = "minidspshd" ]; then
+    pluginPath="/data/plugins/audio_interface"
+    pluginInputs="/volumio/app/plugins/music_service/inputs/index.js"
+  	echo "Add special config for minidsp ..."
+    if [ $(grep -c "input-defaults" $pluginInputs ) -eq 0 ]; then
+      sed -i "s/self.commandRouter.volumioToggle();/self.commandRouter.volumioToggle();\nthis.commandRouter.executeOnPlugin('audio_interface', 'input-defaults', 'setDefaultValues');/g" $pluginInputs
+    else
+      echo "Plugin inputs already updated ..."
+    fi
+  else
+    echo "Detected non-miniDSP device. Plugin is not compatible with your device. [$variant]"
+  fi
 
-    		if [ $variant = "minidspshd" ]; then
-      			pluginPath="/data/plugins/audio_interface"
-      			pluginInputs="/volumio/app/plugins/music_service/inputs/index.js"
-			echo "Add special config for minidsp ..."
-      			if [ $(grep -c "input-defaults" $pluginInputs ) -eq 0 ]; then
-        			sed -i "s/self.commandRouter.volumioToggle();/self.commandRouter.volumioToggle();\nthis.commandRouter.executeOnPlugin('audio_interface', 'input-defaults', 'setDefaultValues');/g" $pluginInputs
-        		else
-        			echo "Plugin inputs already updated ..."
-      			fi
-    		fi
-	else
-		echo "Plugin already exists, not continuing." 
-	fi
 	rm $INSTALLING
 
 	# Required to end the plugin install
